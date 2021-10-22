c1e0220a8fd9dd47bbfcd27cc3c801f039a2ca59
lismgmk <lismgmk2@gmail.com>
2021-10-23 01:05:23 +0300

    second

---
+++ src/components/Input.tsx
@@ -0,0 +1,37 @@
+import React, {ChangeEvent, useState} from 'react';
+
+function Input(props: any) {
+
+    // const {charCode, name, number, setNumber, setAtribut} = props
+    //
+    // const [current, setCurrent] = useState<any>(0)
+    // //     currency: name,
+    // //     value: 0,
+    // // });
+    //
+    //
+    //
+    // const countInput = (event: ChangeEvent<HTMLInputElement>) => {
+    //     // @ts-ignore
+    //     setAtribut(event.target.dataset)
+    //     setCurrent(+event.target.value)
+    //     setNumber(+event.target.value)
+    // }
+
+    return (
+        <div>
+            {/*{console.log(charCode)}*/}
+            {/*{console.log(number)}*/}
+            {/*{charCode}*/}
+            {/*<span>{name}</span>*/}
+            {/*<input*/}
+            {/*    data-currunsy={charCode}*/}
+            {/*    value={number? number : current}*/}
+            {/*    onChange={(e) => {countInput(e)}}*/}
+            {/*    type='text'*/}
+            {/*/>*/}
+        </div>
+    )
+}
+
+export default Input;
